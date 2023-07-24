a2aa27db30cf173eef8f534a1343bb3efd03fff1
Rafael Ayala <rafayala30@gmail.com>
2023-07-24 11:16:42 -0400

    maven project set up.

---
+++ reciperunway/reciperunway/src/main/java/com/rayala30/reciperunway/ReciperunwayApplication.java
@@ -0,0 +1,13 @@
+package com.rayala30.reciperunway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciperunwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciperunwayApplication.class, args);
+	}
+
+}
---
+++ reciperunway/reciperunway/src/test/java/com/rayala30/reciperunway/ReciperunwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayala30.reciperunway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciperunwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
