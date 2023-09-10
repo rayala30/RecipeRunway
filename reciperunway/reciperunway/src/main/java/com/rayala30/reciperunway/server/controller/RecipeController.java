ef0d8e20766f6f1dc0c489d8b06ee3a07a8b88c2
Rafael Ayala <rafayala30@gmail.com>
2023-09-10 13:07:37 -0400

    recipe controller progress

--- reciperunway/reciperunway/src/main/java/com/rayala30/reciperunway/server/controller/RecipeController.java
+++ reciperunway/reciperunway/src/main/java/com/rayala30/reciperunway/server/controller/RecipeController.java
@@ -1,7 +1,16 @@
 package com.rayala30.reciperunway.server.controller;
 
 
+import com.rayala30.reciperunway.server.dao.RecipeCardDao;
+import com.rayala30.reciperunway.server.model.RecipeCard;
+import com.rayala30.reciperunway.server.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -9,5 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api/v1/recipes")
 public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @Autowired
+    private RecipeCardDao recipeCardDao;
+
+    @GetMapping
+    public List<RecipeCard> list() {
+        return recipeCardDao.getRecipes();
+    }
+
+
+
+
 }
